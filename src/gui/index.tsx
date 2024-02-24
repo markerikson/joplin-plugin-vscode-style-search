9e5b85b1540fefa71359996502f48070277ef7b2
Mark Erikson <mark@isquaredsoftware.com>
2024-02-24 14:13:06 -0500

    Get basic search requests working

--- src/gui/index.tsx
+++ src/gui/index.tsx
@@ -4,17 +4,17 @@ import * as ReactDOM from 'react-dom/client'
 
 import { ChannelClient, ChannelErrors, PostMessageTarget } from '../shared/channelRpc'
 
-import type { HandlerType } from '../index'
+import type { HandlerType, Note } from '../index'
 
 const target: PostMessageTarget = {
   postMessage: async (message: any) => {
-    console.log('Client postMessage: ', message)
+    // console.log('Client postMessage: ', message)
     webviewApi.postMessage(message)
   },
   onMessage(listener) {
-    console.log('Client onMessage listener: ', listener)
+    // console.log('Client onMessage listener: ', listener)
     webviewApi.onMessage((originalMessage) => {
-      console.log('Client onMessage: ', originalMessage)
+      // console.log('Client onMessage: ', originalMessage)
       listener({ source: target, data: originalMessage.message })
     })
   },
@@ -28,19 +28,38 @@ const client = new ChannelClient<HandlerType>({
 })
 
 function App() {
-  const [res, setRes] = useState(0)
-  const handleClick = async () => {
-    const res = await client.stub.add(1, 2)
-    console.log('Result: ', res)
-    setRes(res)
+  const [searchText, setSearchText] = useState('')
+  const [titlesOnly, setTitlesOnly] = useState(false)
+  const [searchResults, setSearchResults] = useState<Note[]>([])
+
+  const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target
+    setSearchText(value)
+
+    let notes: Note[] = []
+    console.log('Search value: ', value)
+    if (value) {
+      notes = await client.stub.search({ searchText: value, titlesOnly })
+      console.log('Search notes: ', notes)
+    }
+
+    setSearchResults(notes)
   }
+
+  const renderedNotes = searchResults.map((note) => {
+    return <li key={note.id}>{note.title}</li>
+  })
+
   return (
     <div>
-      <div style={{ color: 'blue', fontSize: 60 }}>Hello World from React + RSPack!!</div>
+      <div style={{ fontWeight: 700, fontSize: 32 }}>Hello World from React + RSPack!!</div>
       <div>
-        <button onClick={handleClick}>Click Me</button>
+        <input type="text" onChange={handleChange} value={searchText} />
       </div>
-      <div>Result: {res}</div>
+      <div>
+        Results:
+        <ul>{renderedNotes}</ul>
+      </div>
     </div>
   )
 }
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,16 @@
 import joplin from 'api'
+import internal = require('stream')
 import { ChannelServer, PostMessageTarget } from './shared/channelRpc'
 import { RpcMethods } from './shared/rpcTypes'
 
+export interface SearchQueryOptions {
+  searchText: string
+  titlesOnly?: boolean
+}
+
 const handler = {
   add: (a: number, b: number): number => a + b,
+  search: searchNotes,
 }
 
 export type HandlerType = typeof handler
@@ -19,6 +26,62 @@ export const createRpcServer = (target: PostMessageTarget) => {
   return server
 }
 
+export interface Note {
+  id: string
+  parent_id: string
+  title: string
+  body: string
+  body_html: string
+  created_time: number
+  updated_time: number
+  source: string
+}
+
+interface SearchResponse {
+  has_more: boolean
+  items: Note[]
+}
+
+async function searchNotes(queryOptions: SearchQueryOptions): Promise<Note[]> {
+  let hasMore = false
+  let allNotes: Note[] = []
+  let page = 1
+
+  const { searchText, titlesOnly } = queryOptions
+
+  const query = titlesOnly ? `title:${searchText}` : searchText
+
+  const fields = ['id', 'title', 'body', 'parent_id', 'is_todo', 'todo_completed', 'todo_due', 'order', 'created_time']
+
+  while (true) {
+    console.log('data call get', ['search'], {
+      query,
+      page,
+      fields,
+    })
+    const res: SearchResponse = await joplin.data.get(['search'], {
+      query,
+      page,
+      fields,
+      limit: 100,
+    })
+    console.log('Search response: ', { query, page })
+    const { items: notes, has_more } = res
+    allNotes = allNotes.concat(notes)
+
+    hasMore = has_more
+    if (!hasMore) {
+      break
+    } else {
+      page++
+    }
+  }
+
+  console.log('Search query: ', queryOptions, 'Found notes: ', allNotes)
+
+  return allNotes
+}
+
 joplin.plugins.register({
   onStart: async function () {
     // eslint-disable-next-line no-console
@@ -31,7 +94,6 @@ joplin.plugins.register({
     await joplin.views.panels.setHtml(
       panel,
       `
-			<div class="container">Loading...</div>
 			<div id="root"></div>
 		`,
     )
@@ -41,20 +103,20 @@ joplin.plugins.register({
 
     const target: PostMessageTarget = {
       postMessage: async (message: any) => {
-        console.log('Server postMessage: ', message)
+        // console.log('Server postMessage: ', message)
         joplin.views.panels.postMessage(panel, message)
       },
       onMessage(listener) {
-        console.log('Server onMessage listener: ', listener)
+        // console.log('Server onMessage listener: ', listener)
         joplin.views.panels.onMessage(panel, (originalMessage) => {
-          console.log('Server onMessage: ', originalMessage)
+          // console.log('Server onMessage: ', originalMessage)
           listener({ source: target, data: originalMessage })
         })
       },
     }
 
     const server = createRpcServer(target)
-    console.log('Starting server')
+    // console.log('Starting server')
     server.start()
   },
 })
